37c9c4cc448922cca3c5964e436cae6df504dc9e
Volodymyr-Benkovskyy <123126442+Volodymyr-Benkovskyy@users.noreply.github.com>
2023-08-17 17:12:58 +0200

    creation  Action counterDecrementAction, resetCounterAction, counterIncrementAction  and counterReducer

--- src/components/redux/Counter/CounterActions.js
+++ src/components/redux/Counter/CounterActions.js
@@ -1,28 +1,40 @@
-import {
-  COUNTER_DECREMENT,
-  COUNTER_INCREMENT,
-  COUNTER_RESET,
-} from "./CounterConstants";
+import { createAction } from "@reduxjs/toolkit";
 
-// створюєм функцію Action для зміни стейту по кліку
-export const counterDecrementAction = (value) => {
-  return {
-    type: COUNTER_DECREMENT,
-    payload: value, // передаєм при виклику функції аргументом 25
-  };
-};
+export const counterDecrementAction = createAction("counter/decrement");
 
-// ств... Action  для обнулення  counter
-export const resetCounterAction = () => {
-  return {
-    type: COUNTER_RESET,
-  };
-};
+export const resetCounterAction = createAction("counter/reset");
 
-//  ств... Action для додавання
-export const counterIncrementAction = (value) => {
-  return {
-    type: COUNTER_INCREMENT,
-    payload: value,
-  };
-};
+export const counterIncrementAction = createAction("counter/increment");
+
+//  ================== версія з redux =================== //
+
+// import {
+//   COUNTER_DECREMENT,
+//   COUNTER_INCREMENT,
+//   COUNTER_RESET,
+// } from "./CounterConstants";
+
+// // створюєм функцію Action для зміни стейту по кліку
+// export const counterDecrementAction = (value) => {
+//   return {
+//     type: COUNTER_DECREMENT,
+//     payload: value, // передаєм при виклику функції аргументом 25
+//   };
+// };
+
+// // ств... Action  для обнулення  counter
+// export const resetCounterAction = () => {
+//   return {
+//     type: COUNTER_RESET,
+//   };
+// };
+
+// //  ств... Action для додавання
+// export const counterIncrementAction = (value) => {
+//   return {
+//     type: COUNTER_INCREMENT,
+//     payload: value,
+//   };
+// };
+
+// creation  Action counterDecrementAction, resetCounterAction, counterIncrementAction  and counterReducer
--- src/components/redux/Counter/counterReducer.js
+++ src/components/redux/Counter/counterReducer.js
@@ -1,23 +1,39 @@
+import { createReducer } from "@reduxjs/toolkit";
+import {
+  counterDecrementAction,
+  counterIncrementAction,
+  resetCounterAction,
+} from "./CounterActions";
+
 //  створення Reducer для Counter
 
-import {
-  COUNTER_DECREMENT,
-  COUNTER_INCREMENT,
-  COUNTER_RESET,
-} from "./CounterConstants";
+// const counterReducer = (state = 200, action) => {
+//   switch (action.type) {
+//     case COUNTER_DECREMENT:
+//       return state - action.payload;
 
-const counterReducer = (state = 200, action) => {
-  switch (action.type) {
-    case COUNTER_DECREMENT:
-      return state - action.payload;
+//     case COUNTER_RESET:
+//       return 0;
+//     case COUNTER_INCREMENT:
+//       return state + action.payload;
+//     default:
+//       return state;
+//   }
+// };
 
-    case COUNTER_RESET:
-      return 0;
-    case COUNTER_INCREMENT:
-      return state + action.payload;
-    default:
-      return state;
-  }
-};
+export const counterReducer = createReducer(150, (builder) => {
+  builder
+    .addCase(counterDecrementAction, (state, { payload }) => {
+      return state - payload;
+    })
+    .addCase(resetCounterAction, () => 150)
+
+    .addCase(counterIncrementAction, (state, { payload }) => {
+      return state + payload;
+    });
+  console.log(builder);
+});
 
 export default counterReducer;
+
+// builder ====>>  це обєкт з методами ==>> addCase , addDefaultCase ,addMatcher
